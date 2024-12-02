3cd655faa8fad636dfce0cd2f5aa9ca27c106d3c
Daniel Rashba <danirdd92@gmail.com>
2024-12-02 22:25:17 +0200

    easy setup

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,13 +10,13 @@ terraform {
 
 provider "grafana" {
   url  = "http://localhost:3000"
-  auth = "admin:prom-operator"
+  auth = "admin:admin"
 }
 
 module "alert_converter" {
   source = "./modules/alert_converter"
 
   prometheus_alerts_file_path = file("${path.module}/files/alerts.yaml")
-  folder_uid                  = "need to create a folder and check the uid via the api before running this!"
-  datasource_uid              = "prometheus"
+  folder_uid                  = "ae5qdfq4wjhmod"
+  datasource_uid              = "PBFA97CFB590B2093"
 }
